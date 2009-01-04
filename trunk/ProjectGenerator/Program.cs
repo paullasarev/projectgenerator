04631a7f764496934e54ec60049aed5247ca42a5
paullasarev@gmail.com <paullasarev@gmail.com@c5ffad6e-da50-11dd-8179-53c65a4d6d95>
2009-01-04 11:53:50 +0000

    initial revision

---
+++ trunk/ProjectGenerator/Program.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using Commons.GetOptions;
+
+//[assembly: AssemblyTitle("ProjectGenerator")]
+//[assembly: AssemblyCopyright("(c)2008 KINT")]
+//[assembly: AssemblyDescription("Template project generator")]
+//[assembly: AssemblyVersion("2.0.0.0")]
+
+[assembly: Commons.About("Generate projects by template")]
+[assembly: Commons.Author("Paul Lasarev")]
+[assembly: Commons.UsageComplement("")]
+[assembly: Commons.AdditionalInfo("")]
+[assembly: Commons.ReportBugsTo("support@kint.ru")]
+
+
+namespace ProjectGenerator
+{
+	public class ProjectGeneratoreOptions : Options
+	{
+		[Option("Template path (required)", "template", "t")]
+		public string TemplatePath;
+
+		[Option("Output path (optional, default='.')", "out", "o")]
+		public string OutputPath = ".";
+
+		[Option(-1, "Define name=value pair (optional)", "name", "n")]
+		public string[] Names;
+
+		[Option("Overwrite output files", "overwrite", "w")]
+		public bool Overwrite = false;
+
+		[Option("Verbose output", "verbose", "v")]
+		public bool Verbose = false;
+
+		[Option("Cache .args.template files", "cache", "c")]
+		public bool CacheArgs = false;
+
+		public ProjectGeneratoreOptions(string[] args) : base(args) { }
+
+		public bool Validate()
+		{
+			if (TemplatePath == null)
+				return false;
+			return true;
+		}
+	}
+
+	public class ProjectGenerator
+	{
+		[STAThread]
+		public static int Main(string[] args)
+		{
+			ProjectGeneratoreOptions options = new ProjectGeneratoreOptions(args);
+			if (!options.Validate())
+			{
+				options.DoHelp();
+				return 1;
+			}
+			TemplateDir template = new TemplateDir(options.TemplatePath);
+			template.Verbose = options.Verbose;
+			template.CacheArgs = options.CacheArgs;
+			Dictionary<string, object> templateArgs = new Dictionary<string, object>();
+			foreach (string pair in options.Names)
+			{
+				int index = pair.IndexOf('=');
+				string name = (index < 0 ? pair : pair.Substring(0, index));
+				string value = (index < 0 ? "" : pair.Substring(index + 1));
+				templateArgs.Add(name, value);
+			}
+			template.Generate(templateArgs, options.OutputPath, options.Overwrite);
+			return 0;
+		}
+	}
+}
---
+++ trunk/ProjectGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProjectGenerator")]
+[assembly: AssemblyDescription("Template project generator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectGenerator")]
+[assembly: AssemblyCopyright("Copyright © KINT 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e6a71f1-b42c-46a6-a3a2-4cf72405d52d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
